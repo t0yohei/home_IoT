3670d93b0f39ece0170a34470c6ea699084065ef
toyo-hei <inuneko690121@gmail.com>
2017-09-09 19:30:56 +0900

    コントローラー名をhomeに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
-  get 'pages/show'
+  root 'home#index'
+  get 'home/show'
 
 end
