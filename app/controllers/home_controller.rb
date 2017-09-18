7c3d827b03a2f0312ca0dc72e70981ce921f7c20
t0yohei <inuneko690121@gmail.com>
2017-09-19 02:06:38 +0900

    コントローラー編集

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,16 @@ class HomeController < ApplicationController
 
   def show
   end
+
+  def forecast_switch
+    @switch = params[:forecast_switch]
+    if @switch == "on"
+      p "test"
+      system('rake switch:forecast_switch_on')
+    end
+  end
+
+  def forecast_params
+    params.permit(:forecast_switch)
+  end
 end
--- lib/tasks/forecast_switch.rake
+++
@@ -1,6 +0,0 @@
-namespace :switch do
-  desc 'do forecast_switch script'
-  task :forecast_switch => :environment do
-    system("python #{Rails.root}/scripts/operation.py")
-  end
-end
