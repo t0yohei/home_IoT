c45b427b952fa65295c165745cb448430c9347f0
toyo-hei <inuneko690121@gmail.com>
2017-10-15 16:28:53 +0900

    コントローラー作成

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,30 +8,49 @@ class HomeController < ApplicationController
   def forecast_switch_on
     @switch = params[:forecast_switch]
       system('rake switch:forecast_switch_on')
-    end
+      render action: :index
   end
 
   def forecast_switch_off
     @switch = params[:forecast_switch]
       system('rake switch:forecast_switch_off')
       render action: :index
-    end
   end
 
   def cool_on
     @switch = params[:air_condition_switch]
-    p "-----cool_on_cont--------"
+    p "-----cool_on_contoroller--------"
+    p @switch
       system('rake switch:cool_on')
+      @message = "cool_on_now"
       render action: :index
-    end
   end
 
-  def cool_on
+  def hot_on
     @switch = params[:air_condition_switch]
-    p "-----cool_on_cont--------"
-      system('rake switch:cool_on')
+    p "-----hot_on_contoroller--------"
+    p @switch
+      system('rake switch:hot_on')
+      @message = "hot_on_now"
       render action: :index
-    end
+  end
+
+  def dry_on
+    @switch = params[:air_condition_switch]
+    p "-----dry_on_contoroller--------"
+    p @switch
+      system('rake switch:dry_on')
+      @message = "dry_on_now"
+      render action: :index
+  end
+
+  def air_off
+    @switch = params[:air_condition_switch]
+    p "-----air_off_contoroller--------"
+    p @switch
+      system('rake switch:air_off')
+      @message = "air_off_now"
+      render action: :index
   end
 
   def forecast_params
