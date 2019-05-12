0d450bfa3f6deac97cb800d8827243a8d9ee7638
t0yohei <inuneko690121@gmail.com>
2019-05-12 12:11:37 +0900

    header部分のコンポーネント化を実施

--- app/javascript/components/home/index.vue
+++ app/javascript/components/home/index.vue
@@ -1,19 +1,13 @@
 <template>
   <div>
-    <div class="container-fluid text-center">
-      <h2>お家のIoT</h2>
-    </div>
+    <index-header></index-header>
 
     <div class="container-fluid">
       <div class="container-fluid col-sm-6">
         <div class="container-fluid">
           <div class="row bg-success switches">
             <h3>CO2濃度取得</h3>
-            <button
-              v-on:click="getCo2Density"
-              class="btn btn-success"
-            >取得
-            </button>
+            <button v-on:click="getCo2Density" class="btn btn-success">取得</button>
           </div>
         </div>
 
@@ -21,45 +15,21 @@
           <div class="row bg-info switches">
             <h3>天気予報アプリの起動</h3>
             <div>
-              <button
-                v-on:click="turnOnWeatherForecast"
-                class="btn btn-info"
-              >天気予報on
-              </button>
-              <button
-                v-on:click="turnOffWeatherForecast"
-                class="btn btn-info"
-              >天気予報off
-              </button>
+              <button v-on:click="turnOnWeatherForecast" class="btn btn-info">天気予報on</button>
+              <button v-on:click="turnOffWeatherForecast" class="btn btn-info">天気予報off</button>
             </div>
           </div>
         </div>
 
         <div class="container-fluid">
           <div class="row bg-warning switches">
-          <h3>エアコンのリモコン</h3>
-          <div>
-            <button
-              v-on:click="turnOnCool"
-              class="btn btn-warning"
-            >冷房
-            </button>
-            <button
-              v-on:click="turnOnWarm"
-              class="btn btn-warning"
-            >暖房
-            </button>
-            <button
-              v-on:click="turnOnDry"
-              class="btn btn-warning"
-            >除湿
-            </button>
-          </div>
-          <button
-            v-on:click="turnOffAirControll"
-            class="btn btn-danger"
-          >エアコン停止
-          </button>
+            <h3>エアコンのリモコン</h3>
+            <div>
+              <button v-on:click="turnOnCool" class="btn btn-warning">冷房</button>
+              <button v-on:click="turnOnWarm" class="btn btn-warning">暖房</button>
+              <button v-on:click="turnOnDry" class="btn btn-warning">除湿</button>
+            </div>
+            <button v-on:click="turnOffAirControll" class="btn btn-danger">エアコン停止</button>
           </div>
         </div>
       </div>
@@ -108,7 +78,7 @@
   text-align: center;
 }
 .control-log {
-  overflow-y:scroll;
+  overflow-y: scroll;
   width: 90%;
 }
 .right-row {
@@ -121,37 +91,43 @@
 </style>
 
 <script>
+import Header from "./index/Header";
+
 export default {
-  data: function () {
+  components: {
+    "index-header": Header
+  },
+
+  data: function() {
     return {
       co2: {
-        time: '',
-        density: ''
-      },
-    }
+        time: "",
+        density: ""
+      }
+    };
   },
   methods: {
-    getCo2Density(){
-      console.log('co2');
+    getCo2Density() {
+      console.log("co2");
     },
-    turnOnWeatherForecast(){
-      console.log('天気予報on');
+    turnOnWeatherForecast() {
+      console.log("天気予報on");
     },
-    turnOffWeatherForecast(){
-      console.log('天気予報off');
+    turnOffWeatherForecast() {
+      console.log("天気予報off");
     },
     turnOnCool() {
-      console.log('冷房on');
+      console.log("冷房on");
     },
     turnOnWarm() {
-      console.log('暖房on');
+      console.log("暖房on");
     },
     turnOnDry() {
-      console.log('除湿on');
+      console.log("除湿on");
     },
     turnOffAirControll() {
-      console.log('エアコン停止');
-    },
+      console.log("エアコン停止");
+    }
   }
 };
 </script>
---
+++ app/javascript/components/home/index/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container-fluid text-center">
+    <h2>お家のIoT</h2>
+  </div>
+</template>
+
+<style scoped></style>
+
+<script lang="ts"></script>
