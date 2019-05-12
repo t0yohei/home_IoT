85294e2e65b3cfc2ac22d6026edf353354cc06ce
t0yohei <inuneko690121@gmail.com>
2019-05-12 18:54:04 +0900

    vue.js化に伴い、不要となったものを削除orコメントアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,27 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #CO2濃度取得
+  # CO2濃度取得
   root 'home#index'
-  get 'home/index'
-  post 'home/index'
-  get 'home/show'
+  # get 'home/index'
+  # post 'home/index'
+  # get 'home/show'
 
-  #スイッチ系
-  post 'home/forecast_switch'
-  post 'home/air_controll'
+  # #スイッチ系
+  # post 'home/control_forecast_switch'
+  # post 'home/air_controll'
 
+  # API
+  namespace :api, format: 'json' do
+    namespace :v1 do
+      namespace :home do
+        get 'get_co2_density'
+        post 'operate_forecast_switch'
+        post 'operate_air_controll'
+      end
+    end
+  end
 end
