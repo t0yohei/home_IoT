d0f275d8cfa6f9499dc0208630a3da112f109000
t0yohei <inuneko690121@gmail.com>
2019-05-14 01:24:34 +0900

    天気予報機能のAPIを実装

--- app/controllers/api/v1/home_controller.rb
+++ app/controllers/api/v1/home_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class Api::V1::HomeController < ApplicationController
-  $message_log = []
+  # ローカル開発のみなので CSRF 対策を向こうにする。
+  # 本番サーバーなどにあげる場合はちゃんと対策する必要がある。
+  protect_from_forgery only: ['operate_forecast_switch']
 
   def get_co2_density
-    operation = 'on'
-    switch_name = 'CO2取得スイッチ'
+    operation_value = 'on'
+    operation_target = 'CO2取得スイッチ'
     begin
       co2 = Co2.get_co2
     rescue StandardError => e
@@ -15,19 +17,29 @@ class Api::V1::HomeController < ApplicationController
 
     result = {
       co2: co2,
-      message: create_message(switch_name, operation)
+      message: create_message(operation_target, operation_value)
     }
     render json: result
   end
 
-  # def forecast_switch
-  #   operation = params[:forecast_switch]
-  #   switch_name = '天気予報スイッチ'
-  #   rake_command = 'rake forecast_switch:' + operation
-  #   system(rake_command)
-  #   create_message(switch_name, operation)
-  #   render action: :index
-  # end
+  def operate_forecast_switch
+    forecast_switch_active_flag = params[:forecast_switch_active_flag]
+    operation_target = '天気予報機能'
+    rake_command = 'rake forecast_switch:' + forecast_switch_active_flag
+    begin
+      system(rake_command)
+      succeded = true
+    rescue StandardError => e
+      # ちゃんとしないと行けないけれどとりあえず破棄する
+      # error_message = "天気予報機能を操作できませんでした"
+      succeded = false
+    end
+    result = {
+      succeded: succeded,
+      message: create_message(operation_target, forecast_switch_active_flag)
+    }
+    render json: result
+  end
 
   # def air_controll
   #   operation = params[:air_controll]
@@ -40,12 +52,10 @@ class Api::V1::HomeController < ApplicationController
 
   private
 
-  def create_message(switch_name, operation)
+  def create_message(operation_target, operation_value)
     time = Time.current.strftime('%Y-%m-%d %H:%M')
     user = current_user ? current_user.email : 'guest'
-    message = '・' + time.to_s + ' に ' + user + ' が  ' + switch_name + ' を ' + operation + ' にしました。'
-    $message_log.unshift(message)
-    message
+    '・' + time.to_s + ' に ' + user + ' が  ' + operation_target + ' を ' + operation_value + ' にしました。'
   end
 
   def forecast_params
